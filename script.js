33a6965eea0a080e2ed43748f6092a841fec6b36
stanley <ou.stanley@yahoo.com>
2022-02-06 01:17:34 -0500

    IntersectionObserver added to landing page for lazy scrolling.

---
+++ script.js
@@ -0,0 +1,20 @@
+const explore = document.querySelector(".explore-div");
+const forge = document.querySelector(".forge-div");
+const footerSignup = document.querySelector(".footer-signup");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
+
+// h2.forEach((header) => {
+observer.observe(explore);
+observer.observe(forge);
+observer.observe(footerSignup);
+// });
