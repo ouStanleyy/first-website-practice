89a28f98d1a3b8a22d09099f0c55f8b485acbe46
stanley <ou.stanley@yahoo.com>
2022-02-16 11:37:23 -0500

    Implemented footer navigation bar hiding through display changing in script.js.

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const explore = document.querySelector(".explore-div");
 const forge = document.querySelector(".forge-div");
 const footerSignup = document.querySelector(".footer-signup-div");
+const footer = document.getElementById("footer");
 
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       entry.target.classList.toggle("show", entry.isIntersecting);
+      console.log(footer);
+      if (entry.target == footerSignup) {
+        console.log("Setting to Flex");
+        footer.style.display = "flex";
+      } else {
+        console.log("Setting to None");
+        footer.style.display = "none";
+      }
     });
   },
   {
@@ -14,7 +23,7 @@ const observer = new IntersectionObserver(
 );
 
 // h2.forEach((header) => {
+observer.observe(footerSignup);
 observer.observe(explore);
 observer.observe(forge);
-observer.observe(footerSignup);
 // });
